88723f06c4dfc607e00c69b87993029447e86152
Going <goinghiggvp@gmail.com>
2025-08-24 19:45:16 +0300

    update files

--- api-gateway/eslint.config.mjs
+++ api-gateway/eslint.config.mjs
@@ -27,6 +27,7 @@ export default tseslint.config(
   {
    rules: {
 		'@typescript-eslint/interface-name-prefix': 'off',
+    "@typescript-eslint/no-unsafe-call": "off"
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
