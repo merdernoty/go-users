d2fa5523f47d6ba014f926d8bccd99f9fbcfc0d1
Going <goinghiggvp@gmail.com>
2025-08-24 19:47:25 +0300

    demo app module

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { AppController } from '@/app.controller'
+import { ServiceDiscoveryModule } from '@/service-discovery'
+import { AuthModule } from '@/modules/auth/auth.module'
+import { UserModule } from '@/modules/user/user.module'
+import { AnimeModule } from './modules/anime/anime.module'
 
 @Module({
-	controllers: [AppController],
-	providers: [AppService]
+	imports: [ServiceDiscoveryModule, AuthModule, UserModule, AnimeModule],
+	controllers: [AppController]
 })
 export class AppModule {}
--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { clusterModeBootstrap } from './cluster'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap()
+
+const main = async () => {
+	const isClusterMode = process.env.CLUSTER_MODE === 'true'
+	if (isClusterMode) {
+		clusterModeBootstrap(bootstrap)
+	} else {
+		bootstrap()
+	}
+}
+
+main()
