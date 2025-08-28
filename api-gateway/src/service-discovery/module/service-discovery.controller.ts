ad1a693499659eeb45d1c4dc02db981a802a008b
Going <goinghiggvp@gmail.com>
2025-08-28 23:52:49 +0300

    remove cron from controller

--- api-gateway/src/service-discovery/module/service-discovery.controller.ts
+++ api-gateway/src/service-discovery/module/service-discovery.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Delete, Get, Post } from '@nestjs/common'
+import { Controller, Get, Post } from '@nestjs/common'
 import { ServiceDiscovery } from './service-discovery'
-import { Cron, CronExpression } from '@nestjs/schedule'
 
 @Controller('service-discovery')
 export class ServiceDiscoveryController {
@@ -16,30 +15,5 @@ export class ServiceDiscoveryController {
 	async addClient(name: string, host: string) {
 		this.serviceDiscovery.addClient(name, host)
 		return { message: 'Client added successfully' }
-	}
-
-	@Cron(CronExpression.EVERY_5_MINUTES)
-	async handleCron() {
-		try {
-		} catch (error) {}
-
-		const info = this.serviceDiscovery.clientsInfo
-		const services = info.keys()
-
-		const newInfo: { name: string; host: string }[] = []
-
-		for (const name of services) {
-			const result = await this.serviceDiscovery.pingService(name)
-
-			const hosts = Object.keys(result).filter(host => result[host])
-
-			hosts.forEach(host => {
-				newInfo.push({ name, host })
-			})
-
-			console.log(`Health check for service ${name}:`, JSON.stringify(result, null, 2))
-		}
-
-		this.serviceDiscovery.healthCheck(newInfo)
 	}
 }
